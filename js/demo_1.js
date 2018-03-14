7714c792573b412526fbe190c184affd9a32ea64
yueshangmx <654704420@qq.com>
2018-03-14 20:29:53 +0800

    update vue_project

--- js/demo_1.js
+++ js/demo_1.js
@@ -1,12 +1,12 @@
 let app1 = new Vue({
   el: "#app1",
   data: {
-    tips: "我们不会保存您的帐号密码，仅采集与您信用相关的信息。您的信息受严密保护，不对外公开",
+    tips: "* 我们不会保存您的帐号密码，仅采集与您信用相关的信息。您的信息将受到严密保护，不会对外公开！",
     account: "",
     password: ""
   },
   methods: {
-    nextbtn: function () {
+    loginbtn: function () {
       if (!(this.account && this.password)) {
         alert("请输入用户名或密码");
       }else if(!$(".check-box").is(':checked')){
